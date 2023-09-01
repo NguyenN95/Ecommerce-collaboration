1e7ea6f0a4b7b61286d55af32df685a265d7089f
NguyenN95 <nguyennguyen7595@gmail.com>
2023-09-01 15:08:05 +0700

    scaffold front end

---
+++ Frontend/Client/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Ecommerce;
+
+namespace Ecommerce;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080/api/") });
+
+        await builder.Build().RunAsync();
+    }
+}
